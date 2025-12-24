35dcd89fe29ed7245ad83a025b16c5fd8c9036a1
unknown <qian942867852@qq.com>
2025-12-24 10:04:04 +0800

    1

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\PlantMap_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\PlantMap_autogen.dir\\ParseCache.txt"
+  "PlantMap_autogen"
+  )
+endif()
