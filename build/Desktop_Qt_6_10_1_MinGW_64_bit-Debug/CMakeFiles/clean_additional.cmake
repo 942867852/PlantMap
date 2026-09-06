913a671f82eafbcdc5b9c08f5b9c1264e8fe1f73
unknown <qian942867852@qq.com>
2026-09-06 17:27:15 +0800

    test

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -5,6 +5,12 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
   "CMakeFiles\\PlantMap_autogen.dir\\AutogenUsed.txt"
   "CMakeFiles\\PlantMap_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\plantcore_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\plantcore_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\plantmap_selftest_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\plantmap_selftest_autogen.dir\\ParseCache.txt"
   "PlantMap_autogen"
+  "plantcore_autogen"
+  "plantmap_selftest_autogen"
   )
 endif()
